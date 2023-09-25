200528286f1ef324e36bc9416e487fac9e4ea822
SREEHARI S <48482060+sree-hari-s@users.noreply.github.com>
2023-09-25 12:16:55 +0530

    Minor tweaks
    Page title added
    wordings changed
    Image size reduced

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ from main import get_recommendations_with_details
 from sklearn.preprocessing import LabelEncoder
 import pickle
 
+
+# --- GENERAL SETTINGS ---
+PAGE_TITLE = "Ecommerce Demo"
+PAGE_ICON = ":wave:"
+st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
+
 model = pickle.load(open('model.pkl', 'rb'))
 # Load the CSV dataset
 @st.cache_data    # Caching for faster reloading
@@ -18,12 +24,12 @@ def main():
     # Load the dataset
     data = load_data()
 
-    st.title('Filters')
+    st.title('Filter Category')
     # Filter products by category
     selected_category = st.selectbox('Select a category:', sorted(data['category'].unique()))
     filtered_data = data[data['category'] == selected_category]
 
-    st.title("Product Recommendations App")
+    st.title("Product Recommendations")
 
     # Create an input field for the user to enter the product ID    
     #product_id = st.number_input("Enter Product ID", min_value=0)
@@ -37,16 +43,23 @@ def main():
     product_id_encoder.fit(data['product_id'])
     product_id = product_id_encoder.transform([selected_product_id])[0]
 
-
     selected_product_details = filtered_data[filtered_data['product_id'] == selected_product_id].iloc[0]
-    st.header("Selected Product Details")
-    st.write(f"Product ID: {selected_product_details['product_id']}")
-    st.write(f"Product Name: {selected_product_details['product_name']}")
-    st.write(f"Category: {selected_product_details['category']}")
-    st.write(f"Rating: {selected_product_details['rating']} ({selected_product_details['rating_count']})")
-    st.image(selected_product_details['img_link'], use_column_width=True)
-    st.write(f"Product Link: [Link]({selected_product_details['product_link']})")
-    
+
+    # Use st.beta_columns to create two columns for details and image
+    details_col, image_col = st.columns(2)
+
+    # Display product details in the left column
+    with details_col:
+        st.header("Selected Product Details")
+        st.write(f"Product ID: {selected_product_details['product_id']}")
+        st.write(f"Product Name: {selected_product_details['product_name']}")
+        st.write(f"Category: {selected_product_details['category']}")
+        st.write(f"Rating: {selected_product_details['rating']} ({selected_product_details['rating_count']})")
+
+    # Display product image in the right column
+    with image_col:
+        st.image(selected_product_details['img_link'], use_column_width=True)
+        st.write(f"Product Link: [Link]({selected_product_details['product_link']})")
     
     # Create a button to trigger the recommendations
     if st.button("Get Recommendations"):
@@ -55,7 +68,7 @@ def main():
 
         # Display recommendations in the Streamlit app
         st.header(f"Recommendations")
-        num_columns = 3  # Number of columns in the grid layout
+        num_columns = 2  # Number of columns in the grid layout
         num_recommendations = len(recommendations)
         num_rows = (num_recommendations - 1) // num_columns + 1
 
@@ -74,7 +87,7 @@ def main():
                     st.write(f"Product Name: {recommendation['product_name']}")
                     st.write(f"Category: {recommendation['category']}")
                     st.write(f"Rating: {recommendation['rating']} ({recommendation['rating_count']})")
-                    st.image(recommendation['img_link'], use_column_width=True)
+                    st.image(recommendation['img_link'], use_column_width=False)
                     st.write(f"Product Link: [Link]({recommendation['product_link']})")
                     recommendation_index += 1
 
